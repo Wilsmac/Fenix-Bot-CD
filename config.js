57c1c0c2ad2a0cdef085bbd61adbb98c44afa45e
WilSMaC <134551880+Wilsmac@users.noreply.github.com>
2023-07-15 15:08:31 -0600

    Add files via upload
    Signed-off-by: WilSMaC <134551880+Wilsmac@users.noreply.github.com>

---
+++ config.js
@@ -0,0 +1,75 @@
+import { watchFile, unwatchFile } from 'fs'
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+import cheerio from 'cheerio'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone' 
+
+global.owner = [
+  ['50258115623', '👑 Fenix- Creador 👑', true],
+  ['51918299647', '💫 Fenix- Collaborator 1 💫', true],
+] 
+
+global.suittag = ['51918299647] 
+global.prems = ['51918299647'] 
+global.reportes_solicitudes = ['51918299647']
+
+global.packname = '(☞ﾟ∀ﾟ)☞'
+global.author = '❥︎𖠌Fᴇɴɪx-Bᴏᴛ-GD𖣘✍︎'
+global.wm = '❥︎𖠌Fᴇɴɪx-Bᴏᴛ-GD𖣘✍︎'
+global.igfg = '❥︎𖠌Fᴇɴɪx-Bᴏᴛ-GD𖣘✍︎'
+global.wait = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
+
+global.imagen1 = fs.readFileSync('./Menu2.jpg')
+global.imagen2 = fs.readFileSync('./src/nuevobot.jpg') 
+global.imagen3 = fs.readFileSync('./src/Pre Bot Publi.png')
+global.imagen4 = fs.readFileSync('./Menu.png')
+global.imagen5 = fs.readFileSync('./src/+18.jpg')
+
+global.mods = [] 
+
+//********Tiempo***************
+global.d = new Date(new Date + 3600000)
+global.locale = 'es'
+global.dia = d.toLocaleDateString(locale, { weekday: 'long' })
+global.fecha = d.toLocaleDateString('es', { day: 'numeric', month: 'numeric', year: 'numeric' })
+global.mes = d.toLocaleDateString('es', { month: 'long' })
+global.año = d.toLocaleDateString('es', { year: 'numeric' })
+global.tiempo = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true })
+//*****************************
+global.wm2 = `▸ ${dia} ${fecha}\n▸ 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝`
+global.gt = '❥︎𖠌Fᴇɴɪx-Bᴏᴛ-GD𖣘✍︎'
+global.mysticbot = '❥︎𖠌Fᴇɴɪx-Bᴏᴛ-GD𖣘✍︎'
+global.md = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'
+global.mysticbot = 'https://github.com/BrunoSobrino/TheMystic-Bot-MD'
+global.waitt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
+global.waittt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
+global.waitttt = '*[❗] Ƈᴀʀɢᴀɴᴅᴏ, ᴀɢᴜᴀʀᴅᴇ ᴜɴ ᴍᴏᴍᴇɴᴛᴏ...*'
+global.nomorown = '51918299647'
+global.pdoc = ['application/vnd.openxmlformats-officedocument.presentationml.presentation', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/msword', 'application/pdf', 'text/rtf']
+global.cmenut = '❖––––––『'
+global.cmenub = '┊✦ '
+global.cmenuf = '╰━═┅═━––––––๑\n'
+global.cmenua = '\n⌕ ❙❘❙❙❘❙❚❙❘❙❙❚❙❘❙❘❙❚❙❘❙❙❚❙❘❙❙❘❙❚❙❘ ⌕\n     '
+global.dmenut = '*❖─┅──┅〈*'
+global.dmenub = '*┊»*'
+global.dmenub2 = '*┊*'
+global.dmenuf = '*╰┅────────┅✦*'
+global.htjava = '⫹⫺'
+global.htki = '*⭑•̩̩͙⊱•••• ☪*'
+global.htka = '*☪ ••••̩̩͙⊰•⭑*'
+global.comienzo = '• • ◕◕════'
+global.fin = '════◕◕ • •'
+global.botdate = `⫹⫺ Date :  ${moment.tz('America/Los_Angeles').format('DD/MM/YY')}` //Asia/Jakarta
+global.bottime = `𝗧 𝗜 𝗠 𝗘 : ${moment.tz('America/Los_Angeles').format('HH:mm:ss')}`//America/Los_Angeles
+global.fgif = {key: { participant : '0@s.whatsapp.net'}, message: { "videoMessage": { "title": wm, "h": `Hmm`, 'seconds': '999999999',  'gifPlayback': 'true',  'caption': bottime, 'jpegThumbnail': fs.readFileSync('./Menu.png')}}}
+global.multiplier = 99
+//*************************
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+unwatchFile(file)
+console.log(chalk.redBright("Update 'config.js'"))
+import(`${file}?update=${Date.now()}`)})
