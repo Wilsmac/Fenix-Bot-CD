c08251605bca3be4f24bc37076f3c28c5f041fda
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-10-14 06:14:06 -0600

    𝐹𝐸𝑁𝐼𝑋-𝐵𝑂𝑇-𝐶𝐷

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -6,11 +6,11 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
   }
   const pesan = args.join` `;
   const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`;
-  let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
+  let teks = `*⺀𝐼 𝑁 𝑉 𝑂 𝐶 𝐴 𝑁 𝐷 𝑂 - 𝐺 𝑅 𝑈 𝑃 𝑂⺀*\n\n❏ ${oi}\n\n❏ *𝐸𝑇𝐼𝑄𝑈𝐸𝑇𝐴𝑆:*\n`;
   for (const mem of participants) {
     teks += `┣➥ @${mem.id.split('@')[0]}\n`;
   }
-  teks += `*└* 𝐁𝐲 𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
+  teks += `*└* 𝐹𝐸𝑁𝐼𝑋-𝐵𝑂𝑇-𝐶𝐷\n\n*▌│█║▌║▌║║▌║▌║▌║█*`;
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
 handler.help = ['tagall <mesaje>', 'invocar <mesaje>'];
