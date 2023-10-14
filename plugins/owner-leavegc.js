c62a1d8b8ce57351d54d305ef0c84884268d01d7
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-10-13 23:01:09 -0600

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn, text, command}) => {
   const id = text ? text : m.chat;
-  await conn.reply(id, smsLeave());
+  await conn.reply(id, `${packname} se despide del grupo, fue genial estar aquí`);
   await conn.groupLeave(id);
 };
 handler.command = /^(salir|leavegc|leave|salirdelgrupo)$/i;
