d2b781812d70005c86a4f72c995d7a7ba4cac2f9
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-10-15 20:14:42 -0600

    Update main.js

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ loadChatgptDB();
 
 /* ------------------------------------------------*/
 
-global.authFile = `MysticSession`;
+global.authFile = `FenixSession`;
 const {state, saveState, saveCreds} = await useMultiFileAuthState(global.authFile);
 const msgRetryCounterMap = (MessageRetryMap) => { };
 const msgRetryCounterCache = new NodeCache()
