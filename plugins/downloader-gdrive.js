bcbf24cf5c2a6e3b42638ceeac0e85655ed8bccd
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-10-23 14:46:48 -0600

    Update downloader-gdrive.js

--- plugins/downloader-gdrive.js
+++ plugins/downloader-gdrive.js
@@ -3,7 +3,7 @@ import {sizeFormatter} from 'human-readable';
 const formatSize = sizeFormatter({
   std: 'JEDEC', decimalPlaces: 2, keepTrailingZeroes: false, render: (literal, symbol) => `${literal} ${symbol}B`});
 const handler = async (m, {conn, args}) => {
-  if (!args[0]) throw '*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*\n\n*- 𝙲𝙾𝚁𝚁𝙾𝙱𝙾𝚁𝙴 𝚀𝚄𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝚂𝙴𝙰 𝚂𝙸𝙼𝙸𝙻𝙰𝚁 𝙰:*\n*◉ https://drive.google.com/file/d/1dmHlx1WTbH5yZoNa_ln325q5dxLn1QHU/view*';
+  if (!args[0]) throw '*[INFORMACIÓN] 𝐸𝑅𝑅𝑂𝑅, 𝑃𝑂𝑅 𝐹𝐴𝑉𝑂𝑅 𝑉𝑈𝐸𝐿𝑉𝐴 𝐴 𝐼𝑁𝑇𝐸𝑁𝑇𝐴𝑅𝐿𝑂*\n\n*- 𝐶𝑂𝑅𝑅𝑂𝐵𝑂𝑅𝐸 𝑄𝑈𝐸 𝐸𝐿 𝐸𝑁𝐿𝐴𝐶𝐸 𝑆𝐸𝐴 𝑆𝐼𝑀𝐼𝐿𝐴𝑅 𝐴:*\n*۞ https://drive.google.com/file/d/1dmHlx1WTbH5yZoNa_ln325q5dxLn1QHU/view*';
   try {
     GDriveDl(args[0]).then(async (res) => {
       conn.reply(m.chat, '𝐷𝑒𝑠𝑐𝑎𝑟𝑔𝑎𝑛𝑑𝑜 𝑠𝑢 𝑎𝑟𝑐ℎ𝑖𝑣𝑜, 𝑒𝑠𝑝𝑒𝑟𝑒 𝑢𝑛 𝑚𝑜𝑚𝑒𝑛𝑡𝑜...\n\n𝐸𝑙 𝑡𝑖𝑒𝑚𝑝𝑜 𝑑𝑒 𝑒𝑠𝑝𝑒𝑟𝑎 𝑝𝑢𝑒𝑑𝑒 𝑣𝑎𝑟𝑖𝑎𝑟 𝑑𝑒𝑝𝑒𝑛𝑑𝑖𝑒𝑛𝑑𝑜 𝑑𝑒𝑙 𝑝𝑒𝑠𝑜 𝑑𝑒𝑙 𝑎𝑟𝑐ℎ𝑖𝑣𝑜, 𝑠𝑖 𝑒𝑙 𝑝𝑒𝑠𝑜 𝑠𝑢𝑝𝑒𝑟𝑎 𝑙𝑜𝑠 100 𝑀𝐵 𝑝𝑢𝑒𝑑𝑒 𝑞𝑢𝑒 𝑠𝑢 𝑎𝑟𝑐ℎ𝑖𝑣𝑜 𝑛𝑜 𝑠𝑒𝑎 𝑒𝑛𝑣𝑖𝑎𝑑𝑜', m);
@@ -11,7 +11,7 @@ const handler = async (m, {conn, args}) => {
       conn.sendFile(m.chat, res.downloadUrl, res.fileName, '', m, null, {mimetype: res.mimetype, asDocument: true});
     });
   } catch (e) {
-    m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*\n\n*- 𝙲𝙾𝚁𝚁𝙾𝙱𝙾𝚁𝙴 𝚀𝚄𝙴 𝙴𝙻 𝙴𝙽𝙻𝙰𝙲𝙴 𝚂𝙴𝙰 𝚂𝙸𝙼𝙸𝙻𝙰𝚁 𝙰:*\n*◉ https://drive.google.com/file/d/1dmHlx1WTbH5yZoNa_ln325q5dxLn1QHU/view*');
+    m.reply('*[INFORMACIÓN] 𝐸𝑅𝑅𝑂𝑅, 𝑃𝑂𝑅 𝐹𝐴𝑉𝑂𝑅 𝑉𝑈𝐸𝐿𝑉𝐴 𝐴 𝐼𝑁𝑇𝐸𝑁𝑇𝐴𝑅𝐿𝑂*\n\n*- 𝐶𝑂𝑅𝑅𝑂𝐵𝑂𝑅𝐸 𝑄𝑈𝐸 𝐸𝐿 𝐸𝑁𝐿𝐴𝐶𝐸 𝑆𝐸𝐴 𝑆𝐼𝑀𝐼𝐿𝐴𝑅 𝐴:*\n*۞ https://drive.google.com/file/d/1dmHlx1WTbH5yZoNa_ln325q5dxLn1QHU/view*');
     console.log(e);
   }
 };
