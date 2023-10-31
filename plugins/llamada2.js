31195e8228a06c7751a1e40eadede962403c7937
UNIFINED <134551880+Wilsmac@users.noreply.github.com>
2023-10-31 10:54:00 -0600

    Fenix

---
+++ plugins/llamada2.js
@@ -0,0 +1,19 @@
+const { generateWAMessageFromContent, proto } = (await import('@whiskeysockets/baileys')).default
+var handler = async(m, { conn, text, usedPrefix, command }) => {
+ 
+  let msg = await generateWAMessageFromContent(m.chat, {  callMessage: {
+   conn.relayMessage(m.chat, {
+  scheduledCallCreationMessage: {
+    callvideoType: 'AUDIO',
+    scheduledTimestampMs: 1698430542887,
+    title: `jola`
+  }
+}, {})
+                                                                                                                                                                                                        
+    }}, { quoted: m }) 
+conn.relayMessage(m.chat, msg.message, {})
+}
+handler.command = /^((llamar|bug2|zzzz))$/i
+handler.owner = true
+
+export default handler
